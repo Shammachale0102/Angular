bade953ab70fda4525832276c98c97924ea9d3aa
Sham Machale <sham.machale0102@gmail.com>
2024-08-13 11:10:20 +0530

    update app

--- src/app/new-service.service.ts
+++ src/app/new-service.service.ts
@@ -1,16 +1,33 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NewServiceService {
+export class NewsServiceService {
 
-  constructor(private http: HttpClient) { }
-  apiurl = "https://newsapi.org/v2/everything?q=apple&from=2024-08-07&to=2024-08-07&sortBy=popularity&apiKey=da0f4427672b41b6884430015550d40f"
+  constructor(private http:HttpClient) { }
 
-  getNews():Observable<any>{
-    return this.http.get(this.apiurl)
+  apiUrl = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-13&sortBy=publishedAt&apiKey=c1d857779fd5489ab705cd1fd8bd33f4"
+
+  apiUrl1 = "https://jsonplaceholder.typicode.com/posts"
+
+  apiUrl2 = "https://jsonplaceholder.typicode.com/comments"
+  
+  getNews():Observable<any>
+  {
+    return this.http.get(this.apiUrl)
+
+  }
+
+  getPosts():Observable<any>
+  {
+    return this.http.get(this.apiUrl1)
+  }
+
+  getComments():Observable<any>
+  {
+    return this.http.get(this.apiUrl2)
   }
 }
--- src/app/news-comp/news-comp.component.ts
+++ src/app/news-comp/news-comp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsServiceService } from '../new-service.service';
 
 @Component({
   selector: 'app-news-comp',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class NewsCompComponent {
 
+  constructor(private service:NewsServiceService){}
+  info:any
+  ngOnInit(){
+    this.service.getNews().subscribe((data: { articles: any; })=>{
+     console.log(data)
+     this.info = data.articles
+    })
+
+   
+  }
 }
